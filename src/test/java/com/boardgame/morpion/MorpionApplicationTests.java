c8c96eca9e9b3bdad3d6d25a4f17737b4b0e9a34
ChristopheThiou <christophe.thiou.simplon@gmail.com>
2025-02-18 13:36:50 +0100

    first commit

---
+++ src/test/java/com/boardgame/morpion/MorpionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgame.morpion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorpionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
